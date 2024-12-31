0fc8939057ba1be99677133093bb0b7b200b36ff
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2024-12-31 08:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (6.94%) - LeetSync

--- 1070-convert-to-base-2/convert-to-base-2.java
+++ 1070-convert-to-base-2/convert-to-base-2.java
@@ -2,6 +2,6 @@ class Solution {
     public String baseNeg2(int N) {
         int num=1;//initialize num =1 
         while (num < N) num = (num<<2)+1;//Construct mask
-        return Integer.toBinaryString(num^(num-N));
+        return Integer.toBinaryString(num^(num-N));//calculate base-2 Representation 
     }
 }
