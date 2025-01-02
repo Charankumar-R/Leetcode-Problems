f615b412402e54d02f1c4c2b9fc9fb242df034cc
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-02 20:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (89.30%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -3,11 +3,15 @@ class Solution {
         if (n == 0) {
             return 1;
         }
+        if (n==1){
+            return 10;
+        }
         int total = 10;
-        int prod = 9;
-        for (int i = 2; i < n + 1; i++) {
-            total += prod * (11 - i);
-            prod *= 11 - i;
+        int a=9,b=9;
+        for (int i = 1; i < n ; i++) {
+            a=a*b;
+            total=total+a;
+            b--;
         }
         return total;
     }
