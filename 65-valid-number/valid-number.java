75c7f1f19bc83e55721effb74a301caeeef5f490
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-08 21:09:11 +0530

    Time: 9 ms (10.03%) | Memory: 44.4 MB (8.59%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,36 +1,42 @@
 class Solution {
     public boolean isNumber(String s) {
-        s = s.trim();
+        // s = s.trim();
 
-        boolean digitSeen = false;
-        boolean dotSeen = false;
-        boolean eSeen = false;
+        // boolean digitSeen = false;
+        // boolean dotSeen = false;
+        // boolean eSeen = false;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        // for (int i = 0; i < s.length(); i++) {
+        //     char c = s.charAt(i);
 
-            if (Character.isDigit(c)) {
-                digitSeen = true;
-            } else if (c == '+' || c == '-') {
-                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
-                    return false;
-                }
-            } else if (c == '.') {
-                if (dotSeen || eSeen) {
-                    return false;
-                }
-                dotSeen = true;
-            } else if (c == 'e' || c == 'E') {
-                if (eSeen || !digitSeen) {
-                    return false;
-                }
-                eSeen = true;
-                digitSeen = false;
-            } else {
-                return false;
-            }
+        //     if (Character.isDigit(c)) {
+        //         digitSeen = true;
+        //     } else if (c == '+' || c == '-') {
+        //         if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+        //             return false;
+        //         }
+        //     } else if (c == '.') {
+        //         if (dotSeen || eSeen) {
+        //             return false;
+        //         }
+        //         dotSeen = true;
+        //     } else if (c == 'e' || c == 'E') {
+        //         if (eSeen || !digitSeen) {
+        //             return false;
+        //         }
+        //         eSeen = true;
+        //         digitSeen = false;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+
+        // return digitSeen;
+        try{
+            Double.valueOf(s);
+            return Character.isDigit(s.charAt(s.length() - 1)) || s.charAt(s.length() - 1) == '.';
+        } catch(Exception e){
+            return false;
         }
-
-        return digitSeen;
     }
 }
