6b7c94ccdb4ffb7435a96bbfd275d23930821bc4
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-08 21:14:22 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (35.80%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,42 +1,50 @@
 class Solution {
     public boolean isNumber(String s) {
-        // s = s.trim();
+        
+        //least Runtime 
+        // Time Complexity O(1)
+        // Space Complexity O(n)
 
-        // boolean digitSeen = false;
-        // boolean dotSeen = false;
-        // boolean eSeen = false;
+        s = s.trim();
 
-        // for (int i = 0; i < s.length(); i++) {
-        //     char c = s.charAt(i);
+        boolean digitSeen = false;
+        boolean dotSeen = false;
+        boolean eSeen = false;
 
-        //     if (Character.isDigit(c)) {
-        //         digitSeen = true;
-        //     } else if (c == '+' || c == '-') {
-        //         if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
-        //             return false;
-        //         }
-        //     } else if (c == '.') {
-        //         if (dotSeen || eSeen) {
-        //             return false;
-        //         }
-        //         dotSeen = true;
-        //     } else if (c == 'e' || c == 'E') {
-        //         if (eSeen || !digitSeen) {
-        //             return false;
-        //         }
-        //         eSeen = true;
-        //         digitSeen = false;
-        //     } else {
-        //         return false;
-        //     }
-        // }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-        // return digitSeen;
-        try{
-            Double.valueOf(s);
-            return Character.isDigit(s.charAt(s.length() - 1)) || s.charAt(s.length() - 1) == '.';
-        } catch(Exception e){
-            return false;
+            if (Character.isDigit(c)) {
+                digitSeen = true;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else if (c == '.') {
+                if (dotSeen || eSeen) {
+                    return false;
+                }
+                dotSeen = true;
+            } else if (c == 'e' || c == 'E') {
+                if (eSeen || !digitSeen) {
+                    return false;
+                }
+                eSeen = true;
+                digitSeen = false;
+            } else {
+                return false;
+            }
         }
+
+        return digitSeen;
+
+        // More runtime 
+
+        // try{
+        //     Double.valueOf(s);
+        //     return Character.isDigit(s.charAt(s.length() - 1)) || s.charAt(s.length() - 1) == '.';
+        // } catch(Exception e){
+        //     return false;
+        // }
     }
 }
