c013a50bf52736827198547933a9879c2f5d34d8
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-09 21:31:18 +0530

    Time: 2 ms (97.82%) | Memory: 42.1 MB (81.87%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,13 +1,21 @@
-import java.math.*;
 class Solution {
-    public String multiply(String num1, String num2) {
-        BigInteger n1 = new BigInteger(num1);
-        BigInteger n2 = new BigInteger(num2);
-        
-        // Multiplying the numbers
-        BigInteger result = n1.multiply(n2);
-        
-        // Returning the result as a string
-        return result.toString();
-    }
+   public String multiply(String num1, String num2) {
+    int m = num1.length(), n = num2.length();
+    int[] pos = new int[m + n];
+   
+    for(int i = m - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= 0; j--) {
+            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0'); 
+            int p1 = i + j, p2 = i + j + 1;
+            int sum = mul + pos[p2];
+
+            pos[p1] += sum / 10;
+            pos[p2] = (sum) % 10;
+        }
+    }  
+    
+    StringBuilder sb = new StringBuilder();
+    for(int p : pos) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+    return sb.length() == 0 ? "0" : sb.toString();
+}
 }
