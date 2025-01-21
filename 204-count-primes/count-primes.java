3ac8dbef81454c1562490167ed72859832fb8d01
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-21 20:37:08 +0530

    Time: 118 ms (24.28%) | Memory: 64.1 MB (15.21%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,17 +1,26 @@
 class Solution {
     public int countPrimes(int n) {
         if (n <= 2) return 0; // No primes less than 2
-
-        boolean[] isNotPrime = new boolean[n];
+        
         int count = 0;
+        int[] prime = new int[n];
+        Arrays.fill(prime, 1); // Assume all numbers are prime
+        prime[0] = 0;
+        prime[1] = 0; // 0 and 1 are not primes
 
-        for (int i = 2; i < n; i++) {
-            if (!isNotPrime[i]) { // If i is a prime number
-                count++;
-                // Mark multiples of i starting from i * i
-                for (long j = (long) i * i; j < n; j += i) {
-                    isNotPrime[(int) j] = true;
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i] == 1) { // If i is prime
+                for (int j = i * i; j < n; j += i) { // Mark multiples of i as non-prime
+                    prime[j] = 0;
                 }
+            }
+        }
+
+        // Count the primes
+        for (int i = 2; i < n; i++) {
+            if (prime[i] == 1) {
+                count++;
             }
         }
 
