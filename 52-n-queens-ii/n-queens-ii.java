5883890290ff5646c25ae7f0f4a9f44c880e933c
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-01-28 14:27:15 +0530

    Time: 2 ms (61.73%) | Memory: 40.8 MB (35.40%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -7,20 +7,25 @@ class Solution {
         return count; 
     }
     public static void count(int n,int arr[][]){
+        //Count incrementation
         count++;
     }
+    //to check the queen is safe
     public static boolean isSafe(int n,int arr[][],int row,int col){
         int i,j;
+        //check if any queen is above
         for(i=row;i>=0;i--){
             if(arr[i][col]==1){
                 return false;
             }
         }
+        //check if any queen is present in top left side
         for(i=row,j=col;i>=0 && j>=0;i--,j--){
             if(arr[i][j]==1){
                 return false;
             }
         }
+        //check if any queen is present in top right side
         for(i=row,j=col;i>=0 && j<n;i--,j++){
             if(arr[i][j]==1){
                 return false;
@@ -31,7 +36,7 @@ class Solution {
     public static void queen(int n,int arr[][],int row){
         int col;
         if(row==n){
-            count(n,arr);
+            count(n,arr);//count method calling
         }
         else{
             for(col=0;col<n;col++){
