08ddd5f2614afb9c59ce48a3a605824f55ba682e
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-02-27 20:15:00 +0530

    Time: 3 ms (39.14%) | Memory: 12.5 MB (55.40%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def minTimeToVisitAllPoints(self, points):
-        res=0
-        x1,y1=points.pop()
-        while points:
-            x2,y2=points.pop()
-            res+=max(abs(y2-y1),abs(x2-x1))
-            x1,y1=x2,y2
+        res=0                                                                   # res = 0
+        x1,y1=points.pop()                  # Start at the last point     [-1, 0]
+        while points:                       # While there are still points left  
+            x2,y2=points.pop()              # Pop the next point           [-1, 0] to [3, 4]
+            res+=max(abs(y2-y1),abs(x2-x1)) # Calculate the time to move   abs(3 - (-1)) = abs(4) = 4 abs(4 - 0) = abs(4) = 4 The time taken is max(4, 4) = 4, so res += 4  Next, we move from [3, 4] to [1, 1]. abs(1 - 3) = abs(-2) = 2 abs(1 - 4) = abs(-3) = 3 The time taken is max(2, 3) = 3, so res += 3. After visiting all the points, the total time will be res = 4 + 3 = 7.
+            x1,y1=x2,y2                     # Update the current point
         return res
