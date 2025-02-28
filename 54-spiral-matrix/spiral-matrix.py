7691837d0f98269e20b35f9b3d4b6e1126b9e916
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-02-28 21:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.15%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -3,13 +3,20 @@ class Solution(object):
         res=[]
 
         while matrix:
+
+            #1-> add first row of matrix
             res+=(matrix.pop(0))
 
+            #2-> append last element of list in order
             if matrix and matrix[0]:
                 for row in matrix:
                     res.append(row.pop())
+
+            #3-> add reverse of last row
             if matrix:
                 res+=(matrix.pop()[::-1])
+
+            #4-> append first element of all rows/lists in reverse
             if matrix and matrix[0]:
                 for row in matrix[::-1]:
                     res.append(row.pop(0))
