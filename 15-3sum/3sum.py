7a987f063bc6b12bd469149b322dce93c3864cfc
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-03-06 22:06:10 +0530

    Time: 655 ms (79.98%) | Memory: 18.4 MB (62.74%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,24 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        #populate with unique i,j,k==0
         trip=[]
+
         nums.sort()
+
+        #first loop through considering i
         for ind,val in enumerate(nums):
+            
+            #if i is same as prev val: has already been evaluated
             if (ind>0) and (val == nums[ind-1]):
                 continue
             
             left=ind+1
             right=len(nums)-1
 
+            #when indx is penultimate left ind(j) == right(k)
             while left<right:
+
                 curr_sum=val+nums[left]+nums[right]
+
                 if curr_sum>0:
                     right-=1
+
                 elif curr_sum<0:
                     left+=1
+
                 else:
                     trip.append([val,nums[left],nums[right]])
+
                     left+=1
+
+                    #keep evaluating for more solutions
                     while (left<right) and (nums[left] == nums[left-1]):
                         left+=1
                     
