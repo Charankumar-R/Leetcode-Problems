5b2a58113a3640e6ebc3251ee09f8feec28c513a
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-05-12 19:46:56 +0530

    Time: 5 ms (71.07%), Space: 43.9 MB (75.9%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long num=x;
+        long rev=0;
+        while(x>0){
+            rev=rev*10 + x%10;
+            x/=10;
+        }
+        return num==rev;
+
+
         // if(x<0){
         //     return false;
         // }
@@ -14,16 +26,5 @@ class Solution {
         //     r--;
         // }
         // return true;
-
-        if(x<0){
-            return false;
-        }
-        long num=x;
-        long rev=0;
-        while(x>0){
-            rev=rev*10 + x%10;
-            x/=10;
-        }
-        return num==rev;
     }
 }
