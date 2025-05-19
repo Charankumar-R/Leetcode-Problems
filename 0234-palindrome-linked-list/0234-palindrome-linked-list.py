fadd28639b8332f8b8f5c8a3b4bf1dec3c5b2c8f
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-05-19 08:31:21 +0530

    Time: 43 ms (29.73%), Space: 34.7 MB (82.72%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         fast = head
