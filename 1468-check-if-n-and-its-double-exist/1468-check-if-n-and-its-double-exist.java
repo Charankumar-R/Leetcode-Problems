62c5cb7acf2980670dae9a10ca33eb6b38055c2a
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-14 08:22:57 +0530

    Time: 2 ms (92.68%), Space: 43.1 MB (50.95%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
         Set<Integer> set = new HashSet<>();
         for(int i: arr){
-            if(set.contains(i * 2) || (i % 2 == 0 && set.contains(i / 2))){
+            if(set.contains(2 * i) || (i % 2 == 0 && set.contains(i / 2))){
                 return true;
             }
             set.add(i);
