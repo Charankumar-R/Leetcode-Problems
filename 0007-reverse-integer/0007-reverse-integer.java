81cf0e33fd23ce771784705180185cc233285c04
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-14 13:44:45 +0530

    Time: 1 ms (81.97%), Space: 41 MB (45.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long rev=0;
+        long dig;
+        while(x!=0){
+            dig = x%10;
+            rev = rev*10 + dig;
+            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) return 0;
+            x/=10; 
+        }
+        return (int) rev;
+    }
+}
