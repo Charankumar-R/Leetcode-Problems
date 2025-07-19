2d7b0c637dddde23a8f82d8d3f6ecc0e1dd924b1
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-19 09:17:25 +0530

    Time: 2 ms (97.37%), Space: 45.3 MB (98.19%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -18,19 +18,17 @@ class Solution {
         List<Double> res = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()){
             int size = queue.size();
             double sum = 0;
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++){
                 TreeNode node = queue.poll();
                 sum += node.val;
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
             }
             res.add(sum / size);
         }
-
         return res;
     }
 }
