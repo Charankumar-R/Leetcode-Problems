2ecdb2bc72794e2a85b6c62b2c78db74b83c2c10
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-28 18:56:03 +0530

    Time: 4 ms (88.17%), Space: 58.6 MB (23.45%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -16,3 +16,18 @@ class Solution {
         return slow;
     }
 }
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Set<Integer> seen = new HashSet<>();
+
+//         for(int num : nums) {
+//             if(seen.contains(num)) {
+//                 return num;
+//             }
+//             seen.add(num);
+//         }
+
+//         return -1; 
+//     }
+// }
