3e35d6050b3bc48f86d673e4f4cfc9346224b54f
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-31 10:06:28 +0530

    Time: 3 ms (99.75%), Space: 45 MB (89.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,18 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> graph = new ArrayList<>();
-        
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int[] pre : prerequisites) {
-            int course = pre[0];
-            int prereq = pre[1];
-            graph.get(prereq).add(course);
-            indegree[course]++;
-        }
-        
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                queue.add(i);
-            }
-        }
-        
-        int completed = 0;
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            completed++;
-            
-            for (int next : graph.get(course)) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    queue.add(next);
-                }
-            }
-        }
-        
-        return completed == numCourses;
+    int[] state; List<Integer>[] g;
+    public boolean canFinish(int n, int[][] pre) {
+        state = new int[n]; g = new List[n];
+        for (int i = 0; i < n; g[i++] = new ArrayList<>());
+        for (int[] p : pre) g[p[1]].add(p[0]);
+        for (int i = 0; i < n; i++) if (!dfs(i)) return false;
+        return true;
+    }
+    boolean dfs(int u) {
+        if (state[u] == 1) return false; 
+        if (state[u] == 2) return true; 
+        state[u] = 1; 
+        for (int v : g[u]) if (!dfs(v)) return false;
+        state[u] = 2; 
+        return true;
     }
 }
