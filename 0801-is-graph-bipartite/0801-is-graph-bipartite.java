1bad79b17d53259d0738da8b346c1f23871d86c2
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-07-31 10:16:11 +0530

    Time: 0 ms (100%), Space: 45.5 MB (11.91%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -2,13 +2,19 @@ class Solution {
     int[] c;
     public boolean isBipartite(int[][] g) {
         c = new int[g.length]; Arrays.fill(c, -1);
-        for (int i = 0; i < g.length; i++) if (c[i] < 0 && !dfs(g, i, 0)) return false;
+        for (int i = 0; i < g.length; i++){
+            if (c[i] < 0 && !dfs(g, i, 0)) {
+                return false;
+            }
+        }
         return true;
     }
     boolean dfs(int[][] g, int u, int col) {
         if (c[u] >= 0) return c[u] == col;
         c[u] = col;
-        for (int v : g[u]) if (!dfs(g, v, 1 - col)) return false;
+        for (int v : g[u]) 
+            if (!dfs(g, v, 1 - col)) 
+                return false;
         return true;
     }
 }
