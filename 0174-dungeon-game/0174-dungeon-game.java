096f1531a0dcee84b77dd00b641c1f4b727fdccc
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-08-01 11:07:13 +0530

    Time: 0 ms (100%), Space: 46 MB (5.51%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solve(int i, int j, int m, int n, int[][] dun, int[][] dp) {
+        if (i >= m || j >= n) return Integer.MAX_VALUE;
+        if (i == m - 1 && j == n - 1) {
+            return Math.max(1, 1 - dun[i][j]);
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+        int right = solve(i, j + 1, m, n, dun, dp);
+        int down = solve(i + 1, j, m, n, dun, dp);
+        int total =Math.min(right, down)- dun[i][j];
+        return dp[i][j] = Math.max(1, total);
+    }
+    
+    public int calculateMinimumHP(int[][] dun) {
+        int m = dun.length, n = dun[0].length;
+        int[][] dp = new int[m][n];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return solve(0, 0, m, n, dun, dp);
+    }
+}
