026a951f7275a2d429d216af708eb1860eebe90c
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-08-01 11:08:08 +0530

    Time: 0 ms (100%), Space: 46 MB (5.51%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,20 +1,17 @@
 class Solution {
-    public int solve(int i, int j, int m, int n, int[][] dun, int[][] dp) {
-        if (i >= m || j >= n) return Integer.MAX_VALUE;
-        if (i == m - 1 && j == n - 1) {
-            return Math.max(1, 1 - dun[i][j]);
-        }
-        if (dp[i][j] != -1) return dp[i][j];
-        int right = solve(i, j + 1, m, n, dun, dp);
-        int down = solve(i + 1, j, m, n, dun, dp);
-        int total =Math.min(right, down)- dun[i][j];
-        return dp[i][j] = Math.max(1, total);
-    }
-    
-    public int calculateMinimumHP(int[][] dun) {
-        int m = dun.length, n = dun[0].length;
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return solve(0, 0, m, n, dun, dp);
+    public int minHealthNeededToReachDest(int i, int j, int n, int m, int[][] mat, int[][] dp){
+        if(i==n || j==m) return dp[i][j] = (int) 1e9;
+        if(i==n-1 && j==m-1) return dp[i][j] = (mat[i][j] < 0) ? 1 - mat[i][j] : 1;
+        if(dp[i][j] != 0) return dp[i][j];
+        int right = minHealthNeededToReachDest(i+1, j, n, m, mat, dp);
+        int down = minHealthNeededToReachDest(i, j+1, n, m, mat, dp);
+        int minHealthReq = Math.min(right, down) - mat[i][j];
+        return dp[i][j] = (minHealthReq <= 0) ? 1 : minHealthReq;
+    } 
+
+    public int calculateMinimumHP(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int[][] dp = new int[n+1][m+1];
+        return minHealthNeededToReachDest(0, 0, n, m, mat, dp);
     }
 }
