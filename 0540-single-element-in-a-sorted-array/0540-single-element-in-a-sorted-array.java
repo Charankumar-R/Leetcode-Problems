c8ebe72bc9a5414afa5cb1cc42bf10e211935540
Charankumar R <117891438+RCKcharan10@users.noreply.github.com>
2025-08-09 20:40:42 +0530

    Time: 15 ms (5.13%), Space: 54.8 MB (5%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int xor = 0;
-        for (int n : nums){
-            xor ^= n;
+        // XOR Approach
+        // int xor = 0;
+        // for (int n : nums){
+        //     xor ^= n;
+        // }
+        // return xor;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        return xor;
+        for(int key : map.keySet()){
+            if(map.get(key) == 1){
+                return key;
+            }
+        }
+        return -1;
     }
 }
